cd79c203240ad7689ca28b35e3efb5df9b0958b0
Gustaw Łączkowski <73334798+gutek00714@users.noreply.github.com>
2025-11-06 21:40:46 +0100

    Update modules.py (cactai, pumpkins)

--- modules.py
+++ modules.py
@@ -24,19 +24,20 @@ def position(x, y):
 		else:
 			break
 
-#PUMPKINS
-def set_list_position(list):
-	x = get_pos_x()
-	y = get_pos_y()
-	list[x][y] = get_entity_type()
+#OLD
+# #PUMPKINS
+# def set_list_position(list):
+# 	x = get_pos_x()
+# 	y = get_pos_y()
+# 	list[x][y] = get_entity_type()
 	
-def list_check(list):
-	for i in range(len(list)):
-		if Entities.Dead_Pumpkin in list[i]:
-			return False
-		elif None in list[i]:
-			return False
-	return True
+# def list_check(list):
+# 	for i in range(len(list)):
+# 		if Entities.Dead_Pumpkin in list[i]:
+# 			return False
+# 		elif None in list[i]:
+# 			return False
+# 	return True
 
 #CARROTS
 def harvest_and_carrots():
@@ -107,22 +108,23 @@ def turn_right(direction):
 	elif direction == West:
 		return North
 
-#CACTUS MEASURE + SORT		
-def measure_cactus():
-	x = measure()
-	n = measure(North)
-	w = measure(West)
-	s = measure(South)
-	e = measure(East)
-	quick_print(x, n, w, s, e)
-	if n != None and x > n:
-		swap(North)
-	elif w != None and x < w:
-		swap(West)
-	elif s != None and x < s:
-		swap(South)
-	elif e != None and x > e:
-		swap(East)
+#OLD
+# #CACTUS MEASURE + SORT		
+# def measure_cactus():
+# 	x = measure()
+# 	n = measure(North)
+# 	w = measure(West)
+# 	s = measure(South)
+# 	e = measure(East)
+# 	quick_print(x, n, w, s, e)
+# 	if n != None and x > n:
+# 		swap(North)
+# 	elif w != None and x < w:
+# 		swap(West)
+# 	elif s != None and x < s:
+# 		swap(South)
+# 	elif e != None and x > e:
+# 		swap(East)
 		
 #def cactus():
 #	for x in range(5):
@@ -139,26 +141,6 @@ def measure_cactus():
 #			if n != None:
 #				if measure() < n:
 #					swap(South)
-
-def cactus():
-	for x in range (5):
-		for y in range(8):
-			position(x+11, y + 6)
-			m = measure()
-			if y < 7:
-				n = measure(North)
-			else:
-				n = 999
-				
-			if x < 4:
-				e = measure(East)
-			else:
-				e = 999
-				
-			if m > n and n <= e:
-				swap(North)
-			elif m > e:
-				swap(East)
 
 #REVERSED DRONE FUNCTION - I TRIED TO HAVE 2 DRONES LOOKING FOR TREASURE IN THE SAME TIME
 def reverse_maze_drone():
@@ -182,6 +164,7 @@ def reverse_maze_drone():
 	harvest()
 
 #DRONE MAZE - JUST UNLOCKED MORE DRONES SO I CAN MULTITASK NOW
+#x = position x, y = position y, size = n x n
 def maze_drone(x, y, size):
 	def inner():
 		change_hat(Hats.Purple_Hat)
@@ -195,20 +178,123 @@ def maze_drone(x, y, size):
 	return inner
 
 #DRONE CACTUS
-def cactus_drone():
-	change_hat(Hats.Cactus_Hat)
-	while True:
-		x = 11
-		position(x, 6)
-		for i in range(5):
-			for j in range(8):
-				harvest()
-				if get_ground_type() == Grounds.Grassland:
-					till()
-				plant(Entities.Cactus)
-				move(North)
-			x += 1
-			position(x, 6)
-		position(x, 6)
-		for i in range(7):
-			cactus()
+#x2 = position x, y = position y, c = number of cacti in column, r = number of cacti in row
+def cactus_drone(x2, y, c, r):
+	def inner2():
+		#nonlocal x, y
+		change_hat(Hats.Cactus_Hat)
+		
+		while True:
+			x = x2
+			#x = 11
+			position(x2, y)
+			for i in range(r):
+				for j in range(c):
+					harvest()
+					if get_ground_type() == Grounds.Grassland:
+						till()
+					plant(Entities.Cactus)
+					move(North)
+				x += 1
+				position(x, y)
+			for i in range(c + r - 1):
+				cactus(x2, y, c, r, i)
+			do_a_flip()
+	return inner2
+			
+def cactus(start_x, start_y, c, r, i):
+	for x in range(r):
+		for y in range(c):
+			position(x + start_x, y + start_y)
+			m = measure()
+			if y < c - 1:
+				n = measure(North)
+			else:
+				n = 999
+				
+			if x < r - 1:
+				e = measure(East)
+			else:
+				e = 999
+				
+			if m > n and n <= e:
+				swap(North)
+			elif m > e:
+				swap(East)
+				
+			if x + y >= c + r - i:
+				break
+
+#DRONE PUMPKIN
+#x = position x, y = position y, c = number of pumpkins in column, r = number of pumpkins in row
+# def pumpkin_drone(start_x, start_y, c, r):
+# 	def inner():
+# 		pumpkin_list = init_pumpkin_list(c, r)
+# 		change_hat(Hats.Pumpkin_Hat)
+# 		while True:
+# 			x = start_x
+# 			#x = 11
+# 			position(start_x, y)
+# 			for i in range(r):
+# 				for j in range(c):
+# 					harvest()
+# 					if get_ground_type() == Grounds.Grassland:
+# 						till()
+# 					plant(Entities.Pumpkin)
+# 					move(North)
+# 				x += 1
+# 				position(x, y)
+			
+			
+# 	return inner	
+
+# def init_pumpkin_list(c, r):
+# 	pumpkin_list = []
+# 	for i in range(r):
+# 		row = [0] * c
+# 		pumpkin_list.append(row)
+# 	return pumpkin_list
+
+def pumpkin_drone2(start_x, start_y, c, r):
+	def inner():
+		change_hat(Hats.Pumpkin_Hat)
+		while True:
+			pumpkin_list = []
+			x = start_x
+			position(start_x, start_y)
+			for i in range(r):
+				for j in range(c):
+					pumpkin_list.append(get_entity_type())
+					harvest()
+					if get_ground_type() == Grounds.Grassland:
+						till()
+					plant(Entities.Pumpkin)
+					use_item(Items.Water)
+					move(North)
+				x += 1
+				position(x, start_y)
+
+			position(start_x, start_y)
+			
+			is_done = False
+			while not is_done:
+			
+				x = start_x
+				for i in range(r):
+					for j in range(c):
+						pumpkin_list.append(get_entity_type())
+						plant(Entities.Pumpkin)
+						move(North)
+					x += 1
+					position(x, start_y)
+					
+				position(start_x, start_y)
+					
+				if Entities.Dead_Pumpkin not in pumpkin_list and Entities.Grass not in pumpkin_list:
+					is_done = True
+				
+				pumpkin_list = []
+					
+			harvest()
+			do_a_flip()
+	return inner
